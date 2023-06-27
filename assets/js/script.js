ac11a8ebb294eec479620cd00f51feb5b33336e4
RobinF98 <>
2023-06-27 15:49:30 +0000

    Display addition question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,21 +10,31 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("You've clicked submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`you clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame("addition");
+
 });
 
 /**
  * The main game loop, called on script load
  * and after processing user answer 
  */
-function runGame() {
+function runGame(gameType) {
 
     //Creates two random ints between 1 and 25 (inclusive)
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`unknown game type: ${gameType}`);
+        throw `unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -43,8 +53,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
 
+    document.getElementById("operator").textContent = "+";
 }
 
 function displaySubtractQuestion() {
